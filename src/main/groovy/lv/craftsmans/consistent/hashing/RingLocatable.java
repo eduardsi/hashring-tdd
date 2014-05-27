22c874b2e6ce8219b5cfc489951d018cec1d37d4
eduardsi <EDUARDSI@EDUARDSs-MacBook-Pro-2.local>
2014-05-27 13:54:30 +0300

    packaging fixes

--- src/main/groovy/lv/craftsmans/consistent/hashing/RingLocatable.java
+++
@@ -1,9 +0,0 @@
-package lv.craftsmans.consistent.hashing;
-
-import com.google.common.hash.Hasher;
-
-public interface RingLocatable {
-
-    void registerHashAttributes(Hasher hasher);
-
-}
--- src/main/groovy/lv/craftsmans/consistent/hashing/RingLocationCalculator.java
+++
@@ -1,7 +0,0 @@
-package lv.craftsmans.consistent.hashing;
-
-public interface RingLocationCalculator {
-
-    <T extends RingLocatable> int locationOnARing(T ringPlaceable);
-
-}
